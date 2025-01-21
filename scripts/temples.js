402da60ca0eab335dd9904087687dc02d022f39c
stevenssebuma <ssebumasteven4@gmail.com>
2025-01-21 18:56:26 +0300

    added content

---
+++ scripts/temples.js
@@ -0,0 +1,18 @@
+const year = document.querySelector('#currentyear');
+const today = new Date();
+year.innerHTML = today.getFullYear();
+
+const lastModified = document.querySelector('#lastModified');
+lastModified.innerHTML = (document.lastModified);
+
+const hamButton = document.querySelector("#hamburger");
+const ul = document.querySelector("ul");
+
+hamButton.addEventListener("click", () => {
+    ul.classList.toggle("open-ul");
+    hamButton.classList.toggle("open");
+});
+
+function changeMenu(menu) {
+    document.getElementById('menuHeading').innerText = menu;
+}
